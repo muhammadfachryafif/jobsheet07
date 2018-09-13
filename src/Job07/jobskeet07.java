d0557e240f16dc85488e88cf68d8ea37a1008b71
WINDOWS 10 <WINDOWS 10@Windows10>
2018-09-13 14:09:13 +0700

    Initial Commit

---
+++ src/Job07/jobskeet07.java
@@ -0,0 +1,17 @@
+package Job07;
+import java.util.Scanner;
+import java.math.BigInteger;
+public class jobskeet07 {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitias : " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21,...dat.\n");
+    }
+     public static void main(String[] args)
+    {    
+        String identitas = "MUHAMMAD FACHRI AFIF/X RPL 2 / 22";
+        tampilJudul(identitas);
+    }
+}
