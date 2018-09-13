cc4b48902793f2b0c63c5b1e909150ec19155ea8
WINDOWS 10 <WINDOWS 10@Windows10>
2018-09-13 14:11:47 +0700

    Tambah Tampil Hasil

--- src/Job07/jobskeet07.java
+++ src/Job07/jobskeet07.java
@@ -30,11 +30,16 @@ public class jobskeet07 {
         }
         return hasil[n-1];
     }
+    private static void tampilHasil(int n, BigInteger hasil)
+    {
+        System.out.println("Bilangan Fibonacci ke-" + n + " : " + hasil );
+    }
      public static void main(String[] args)
     {    
         String identitas = "MUHAMMAD FACHRI AFIF/X RPL 2 / 22";
         tampilJudul(identitas);
         int n = tampilInput();
         BigInteger hasil = fibo(n);
+        tampilHasil(n, hasil);
     }
 }
